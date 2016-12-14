7ab51ea36f2485d7ea94a813a44abe79f30d7bf7
cometeor <cometliao@gmail.com>
2016-12-14 12:20:48 +0800

    Add comments.

--- hub.go
+++ hub.go
@@ -1,19 +1,28 @@
 package hub
 
+// Subject a pub/sub subject.
 type Subject interface {
 	Publish(message interface{})
 	Subscribe() Subscriber
 }
 
+// Subscriber a subscriber to a subject.
+// Calling Close() is necessary
+// or it will lead to memory leak.
 type Subscriber interface {
 	Receive() <-chan interface{}
 	Close()
 }
 
+// Stopper provides Stop() interface for PubSub
+// to cleanup and flush suspending messages.
 type Stopper interface {
-	Stop() <- chan bool
+	Stop() <-chan bool
 }
 
+// PubSub holds subjects,
+// Publishers and Subscribers can communicate
+// with each other via the same subject.
 type PubSub interface {
 	Stopper
 	Subject(key string) Subject
@@ -21,13 +30,13 @@ type PubSub interface {
 
 // ------------------------------
 
+// EmptyStopper provides an empty implementation of Stopper.
 type EmptyStopper struct {
-
 }
 
-func (s *EmptyStopper) Stop() <- chan bool {
+// Stop does nothing.
+func (s *EmptyStopper) Stop() <-chan bool {
 	c := make(chan bool)
 	close(c)
 	return c
 }
-
